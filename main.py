ae64b42af7baae3b5c8ada2a3d3eda8f17e95a99
ABMR123 <auggieroehle@gmail.com>
2024-12-14 11:11:11 -0500

    this is a thing

--- main.py
+++ main.py
@@ -1,87 +1,97 @@
 import hashlib
 import time
-
+import socket
+import threading
+import json
 
 class Block:
-    def __init__(self, index, previous_hash, timestamp, data, hash, nonce=0):
+    def __init__(self, index, previous_hash, timestamp, data, hash):
         self.index = index
         self.previous_hash = previous_hash
         self.timestamp = timestamp
         self.data = data
         self.hash = hash
-        self.nonce = nonce
 
-    def __str__(self):
-        return f"Block {self.index}: {self.data}, Hash: {self.hash}, Previous Hash: {self.previous_hash}"
+def calculate_hash(index, previous_hash, timestamp, data):
+    value = str(index) + str(previous_hash) + str(timestamp) + str(data)
+    return hashlib.sha256(value.encode('utf-8')).hexdigest()
 
+def create_genesis_block():
+    return Block(0, "0", int(time.time()), "Genesis Block", calculate_hash(0, "0", int(time.time()), "Genesis Block"))
 
-class Blockchain:
-    def __init__(self):
-        self.chain = []
-        self.current_data = []
-        self.create_genesis_block()
+def create_new_block(previous_block, data):
+    index = previous_block.index + 1
+    timestamp = int(time.time())
+    hash = calculate_hash(index, previous_block.hash, timestamp, data)
+    return Block(index, previous_block.hash, timestamp, data, hash)
 
-    def create_genesis_block(self):
-        """Create the first block in the blockchain (genesis block)."""
-        genesis_block = Block(0, "0", time.time(), "Genesis Block", self.hash_block("0", time.time(), "Genesis Block"))
-        self.chain.append(genesis_block)
+class SupplyChainData:
+    def __init__(self, product_id, location, status):
+        self.product_id = product_id
+        self.location = location
+        self.status = status
 
-    def add_block(self, data):
-        """Add a new block to the chain."""
-        previous_block = self.chain[-1]
-        new_index = previous_block.index + 1
-        timestamp = time.time()
-        new_block = Block(new_index, previous_block.hash, timestamp, data,
-                          self.hash_block(previous_block.hash, timestamp, data))
-        self.chain.append(new_block)
+class OrderData:
+    def __init__(self, order_id, product_id, quantity, status):
+        self.order_id = order_id
+        self.product_id = product_id
+        self.quantity = quantity
+        self.status = status
 
-    def hash_block(self, previous_hash, timestamp, data):
-        """Hash the block's contents using SHA-256."""
-        value = f"{previous_hash}{timestamp}{data}".encode('utf-8')
-        return hashlib.sha256(value).hexdigest()
+# Blockchain Network
+blockchain = [create_genesis_block()]
+peers = set()
 
-    def is_chain_valid(self):
-        """Check the integrity of the blockchain."""
-        for i in range(1, len(self.chain)):
-            current_block = self.chain[i]
-            previous_block = self.chain[i - 1]
-            if current_block.hash != self.hash_block(previous_block.hash, current_block.timestamp, current_block.data):
-                return False
-            if current_block.previous_hash != previous_block.hash:
-                return False
-        return True
+def add_block(data):
+    global previous_block
+    new_block = create_new_block(previous_block, vars(data))
+    blockchain.append(new_block)
+    previous_block = new_block
+    broadcast_block(new_block)
 
-    def proof_of_work(self, block, difficulty=4):
-        """Simple Proof of Work algorithm."""
-        block.nonce = 0
-        while not block.hash.startswith('0' * difficulty):
-            block.nonce += 1
-            block.hash = self.hash_block(block.previous_hash, block.timestamp, block.data + str(block.nonce))
-        return block
+def broadcast_block(block):
+    for peer in peers:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((peer, 5000))
+                s.sendall(json.dumps(vars(block)).encode('utf-8'))
+        except Exception as e:
+            print(f"Failed to send block to {peer}: {e}")
 
-    def print_chain(self):
-        """Print the entire blockchain."""
-        for block in self.chain:
-            print(block)
+def handle_connection(conn, addr):
+    data = conn.recv(1024)
+    if data:
+        block_data = json.loads(data)
+        new_block = Block(**block_data)
+        blockchain.append(new_block)
+    conn.close()
 
+def node_listener():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind(('0.0.0.0', 5000))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            threading.Thread(target=handle_connection, args=(conn, addr)).start()
 
-# Example usage:
 if __name__ == "__main__":
-    blockchain = Blockchain()
+    previous_block = blockchain[0]
 
-    # Add some blocks with data
-    blockchain.add_block("Block 1 Data")
-    blockchain.add_block("Block 2 Data")
+    # Start the node listener thread
+    threading.Thread(target=node_listener).start()
 
-    # Perform proof of work on the last block
-    last_block = blockchain.chain[-1]
-    blockchain.proof_of_work(last_block)
+    # Add new blocks with supply chain data
+    supply_chain_data = SupplyChainData("product123", "Factory", "Manufactured")
+    add_block(supply_chain_data)
 
-    # Print the blockchain
-    blockchain.print_chain()
+    # Add new order
+    order_data = OrderData("order789", "product123", 100, "Placed")
+    add_block(order_data)
 
-    # Validate the blockchain
-    if blockchain.is_chain_valid():
-        print("The blockchain is valid.")
-    else:
-        print("The blockchain is not valid.")
+    # Update order status to 'Fulfilled'
+    order_data_fulfilled = OrderData("order789", "product123", 100, "Fulfilled")
+    add_block(order_data_fulfilled)
+
+    print("Supply Chain Blockchain with Orders created!")
+    for block in blockchain:
+        print(f"Block {block.index}: {block.data}")
